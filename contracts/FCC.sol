5040dac023bde340a5c4d60c260dbeca997e5adb
sanjay-del <sanjaypantha.np@gmail.com>
2022-10-16 17:16:47 +0545

    Initial commit

---
+++ contracts/FCC.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+import '@openzeppelin/contracts/access/Ownable.sol';
+contract FCC {
+event ParticipatorUser(address indexed participator);
+event Claimers(address indexed claimer);
+//  constructor() {
+//         participator = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+//         emit Participator();
+//     }
+}
---
+++ contracts/Treasury.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+import '@openzeppelin/contracts/access/Ownable.sol';
+// import '@openzeppelin/contracts/finance/VestingWallet.sol';
+
+contract Treasury is Ownable{
+    uint256 private balace;
+    // event 
+    // function treasuryBalance() public view returns (uint256){
+    //   return  address(this).balace;
+    // }
+}
