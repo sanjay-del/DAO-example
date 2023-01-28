ce1ffb17406dc154cfd772945cebdd4ed9c76378
anupamakoirala <anukoirala.96@hotmail.com>
2023-01-28 21:38:50 +0545

    hardhat configuration

--- contracts/FCC.sol
+++ contracts/FCC.sol
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
-import '@openzeppelin/contracts/access/Ownable.sol';
+pragma solidity 0.8.10;
 contract FCC {
 event ParticipatorUser(address indexed participator);
 event Claimers(address indexed claimer);
-//  constructor() {
-//         participator = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
-//         emit Participator();
-//     }
+
+struct Proposal{
+    string name;
+    string description;
+    uint256 fund;
+    uint8 proposalId;
+}
+
+mapping(uint256=>Proposal) proposals;
+
+
+
+ 
+ function addProposal( string _name,string _description, uint256 _fund){
+    
+
+ }
 }
---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+module.exports={
+    solidity:{
+        version:"0.8.10",
+        settings:{
+            optimizer:{
+                enabled:false,
+
+            }
+        }
+
+    },
+    paths:{
+        sources:"./contracts",
+        tests:"./test",
+        artifacts:"./artifacts",
+    }
+}
